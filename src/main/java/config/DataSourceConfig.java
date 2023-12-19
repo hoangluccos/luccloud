23a7d314a5c3e3723f213b40a0c66fbe85c4d504
hoangluccos <94107943+hoangluccos@users.noreply.github.com>
2023-12-20 00:00:36 +0700

    hi

---
+++ src/main/java/config/DataSourceConfig.java
@@ -0,0 +1,18 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DataSourceConfig {
+	@Bean
+	public DriverManagerDataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+	    dataSource.setUrl("jdbc:mysql://google/occupationdb?cloudSqlInstance=cloudnhom12:asia-southeast1:jobseeking&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=root&password=");
+	    dataSource.setUsername("root");
+	    dataSource.setPassword("");
+	    return dataSource;
+	}
+}
